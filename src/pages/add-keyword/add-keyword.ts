20c69f05ad798bbcf567d1228f39ac7f1a114139
Tony Lucas <tony3lucas@hotmail.fr>
2017-03-24 09:21:00 +0100

    Fix Google Autocomplete route

--- src/pages/add-keyword/add-keyword.ts
+++ src/pages/add-keyword/add-keyword.ts
@@ -3,6 +3,7 @@ import { KeywordsProvider } from '../../providers/keywords';
 import { Router } from '@angular/router'
 import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 import { Keyword } from '../../model/keyword';
+import { AppComponent } from '../../app/app.component';
 
 
 @Component({
@@ -11,8 +12,8 @@ import { Keyword } from '../../model/keyword';
 })
 export class AddKeyword {
 
-    selectedKeyword: Object;
-    googleGeoCode: string = "http://localhost:3000/google-autocomplete/:keyword";
+    private selectedKeyword: Object;
+    private googleGeoCode: string = AppComponent.API_ENDPOINT + 'google-autocomplete/:keyword';
 
     constructor(public keywordsProvider: KeywordsProvider, private router: Router) {
         this.selectedKeyword = {
